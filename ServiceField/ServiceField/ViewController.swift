01335d315dcb8c76f87dffdeac5f96dd01891c51
Mike Salinas <nmsalinas@student.fullsail.edu>
2023-01-04 13:41:13 -0500

    created app
    beginning process

---
+++ ServiceField/ServiceField/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ServiceField
+//
+//  Created by Natividad Michael Salinas II on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
